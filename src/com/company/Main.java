fb84f4cdbbb96aa11cdad2e3350e836c76ea367f
javiOlaya <jjosaez@gmail.com>
2019-10-30 05:22:08 +0100

    empezar a resolver la posibilidad de solucion imaginaria

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+//ACTIVIDAD 3. Crea un algoritmo que pueda resolver una ecuación de segundo grado (x^2 + x + n)
+     double solucion1=0,solucion2=0,n=0,x1=0,x2=0;
+     Scanner scanner=new Scanner(System.in);
+        System.out.println("Programa para resolver ecuaciòn de 2ª grado x²+x+n: ");
+        System.out.print("Dame el valor n: ");
+        n=scanner.nextDouble();
+        double raiz=1-(4*n);
+        if (raiz<0){
+
+        }
+        else{}
+    }
+}
