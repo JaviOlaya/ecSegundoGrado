b4f1b5d9bdcb21d1cfbaca80e5de9184af78ff6a
javiOlaya <jjosaez@gmail.com>
2019-10-30 05:57:11 +0100

    acabadas las posibilidad real y la imaginaria

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,19 @@ public class Main {
         n=scanner.nextDouble();
         double raiz=1-(4*n);
         if (raiz<0){
-
+            double raiz1= raiz*(-1.0);
+            double img = Math.sqrt(raiz)/2;
+            double sol_real =Math.pow(-0.5,2)-Math.pow(img,2);
+            double sol_img=2*sol_real*img;
+            System.out.println("Solución 1 parte real"+sol_real+" + "+sol_img+" i");
+            System.out.println("Solución 1 parte real"+sol_real+" - "+sol_img+" i");
         }
-        else{}
+        else{
+            x1=(-1+Math.sqrt(raiz))/2;
+            x2=(-1-Math.sqrt(raiz))/2;
+            solucion1=Math.pow(x1,2)+x1+n;
+            solucion2=Math.pow(x2,2)+x2+n;
+            System.out.println("Los resultados de la función: "+"sol1: "+solucion1+" sol2: "+solucion2);
+        }
     }
 }
